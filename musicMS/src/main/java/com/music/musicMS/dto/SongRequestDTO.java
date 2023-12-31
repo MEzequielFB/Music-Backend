c54f62954e3e8bb2fa9bc4015ba81cb0f689092f
Ezequiel Frisone <ezequiel_frisone@hotmail.com>
2023-12-31 19:02:41 -0300

    add song controller tests

--- musicMS/src/main/java/com/music/musicMS/dto/SongRequestDTO.java
+++ musicMS/src/main/java/com/music/musicMS/dto/SongRequestDTO.java
@@ -7,9 +7,15 @@ import com.music.musicMS.model.Album;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class SongRequestDTO {
 	@NotNull(message = "name shouldn't be null")
 	@NotBlank(message = "name shouldn't be empty")
--- musicMS/src/main/java/com/music/musicMS/dto/SongResponseDTO.java
+++ musicMS/src/main/java/com/music/musicMS/dto/SongResponseDTO.java
@@ -5,10 +5,12 @@ import java.util.List;
 import com.music.musicMS.model.Song;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
+@Builder
 @NoArgsConstructor
 @AllArgsConstructor
 public class SongResponseDTO {
