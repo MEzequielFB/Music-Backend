596c502f4deef31f5768a5ac54b3b67a82fd38f1
Ezequiel Frisone <ezequiel_frisone@hotmail.com>
2024-01-10 18:58:56 -0300

    change product response

--- merchandisingMS/src/main/java/com/music/merchandisingMS/dto/ProductResponseDTO.java
+++ merchandisingMS/src/main/java/com/music/merchandisingMS/dto/ProductResponseDTO.java
@@ -1,5 +1,7 @@
 package com.music.merchandisingMS.dto;
 
+import java.util.List;
+
 import com.music.merchandisingMS.model.Product;
 
 import lombok.AllArgsConstructor;
@@ -15,10 +17,14 @@ public class ProductResponseDTO {
 	private Integer id;
 	private String name;
 	private Double price;
+	private Integer stock;
+	private List<TagResponseDTO> tags;
 	
 	public ProductResponseDTO(Product product) {
 		this.id = product.getId();
 		this.name = product.getName();
 		this.price = product.getPrice();
+		this.stock = product.getStock();
+		this.tags = product.getTags().stream().map( TagResponseDTO::new ).toList();
 	}
 }
